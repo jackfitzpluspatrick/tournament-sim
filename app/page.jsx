03e4348a50c0bb152eabf32607c4557e8349fc09
Jack FitzPatrick <jfitzpatrick@Jacks-Air-13.lan>
2026-03-07 17:23:20 -0500

    restore homepage

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main style={{ padding: 40 }}>
+        <h1>Tournament Simulator</h1>
+        <p>Welcome! Start the quiz to generate your bracket.</p>
+      </main>
+    );
+  }
+  
