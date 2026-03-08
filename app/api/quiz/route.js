dc4e0a4f9688b4678b34b8fd2800e64a3aceb3fa
Jack FitzPatrick <jfitzpatrick@Jacks-Air-13.lan>
2026-03-07 20:02:54 -0500

    update quiz route to use upstash

--- app/api/quiz/route.js
+++ app/api/quiz/route.js
@@ -1,13 +1,17 @@
+import { Redis } from "@upstash/redis";
 import { v4 as uuid } from "uuid";
 
-let store = {}; // temporary in-memory store
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 // Save quiz answers
 export async function POST(req) {
   const body = await req.json();
   const id = uuid();
 
-  store[id] = body; // save quiz answers
+  await redis.set(id, body);
 
   return Response.json({ resultId: id });
 }
@@ -17,7 +21,9 @@ export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
 
+  const quizAnswers = await redis.get(id);
+
   return Response.json({
-    quizAnswers: store[id] || {}
+    quizAnswers: quizAnswers || {}
   });
 }
